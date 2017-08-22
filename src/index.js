631bae25e2103a1e49339106f1e53e120d7d361e
Ramprasath Babu <ramprasath.technology@gmail.com>
2017-08-22 17:36:51 -0500

    displaying video and video list

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search-bar';
+import VideoList from './components/video-list';
+import VideoDetail from './components/video-detail';
+
 
 const API_KEY = 'AIzaSyAp4hDsbDHNYF05Hk4cMF2jW2YtZ2drXsM';
 
-const App = () => {
-    return(
-        <div>
-            <SearchBar />
-        </div>
-    ) 
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'Bryan'}, (videos) => {
+            this.setState({videos});
+            console.log(this.state.videos);
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar /> 
+                <VideoDetail video={this.state.videos[0]} />     
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.start'));
